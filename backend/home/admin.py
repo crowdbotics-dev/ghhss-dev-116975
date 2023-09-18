a5e917f5f175cc583509ca848334bbb270499740
crowdbotics-dev <engineering@crowdbotics.com>
2023-09-18 06:50:32 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bvhffh
+admin.site.register(Bvhffh)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Bvhffh(models.Model):
+    'Generated Model'
+    jfjf = models.BigIntegerField()
+    yrur = models.BigIntegerField()
